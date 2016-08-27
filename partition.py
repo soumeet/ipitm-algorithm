c6605d071e12906643d6681cf83fbe2fc93b2d23
soumeet <soumeet.basak@gmail.com>
2016-08-27 18:41:51 +0530

    transaction_list sorted by SOT
    SOT: size of transaction

--- partition.py
+++ partition.py
@@ -2,9 +2,9 @@ import sys
 import csv
 
 dataset=sys.argv[1]
-transactions=[[0,[]]]
+transactions=[[0,[],0]] #transactions=[[tid, items[], sot]]
 no_of_transactions=0
-partition=[]
+partition=[[0,[0,[]],0]] #partition=[pid, [tid, items[]], sot]
 def read_transactions():
 	with open(dataset, newline='') as csvfile:
             spamreader = csv.reader(csvfile)
@@ -14,10 +14,10 @@ def read_transactions():
                 for i in row:
                     item=int(i)
                     transaction_row.append(item)
-                transactions.append([tid,transaction_row])
+                    transaction_size=len(transaction_row)
+                transactions.append([tid,transaction_row,transaction_size])
                 tid +=1
 
 read_transactions()
-no_of_transaction=len(transactions)
-#print(no_of_transaction)
-#print(transactions)
+no_of_transactions=len(transactions)
+#print(no_of_transactions)
