a7520a2b43c6a98b3af95328141bc814c65b40f5
soumeet <soumeet.basak@gmail.com>
2016-08-30 01:02:26 +0530

    partition table implemented, lower order of loop

--- test5.py
+++ test5.py
@@ -1,11 +1,12 @@
 import csv
+import bisect
 transaction=[]
-transaction_part=[]
-item=[[0,[]]]
-c2=1
+part=[]
+item=[]
 def transactions():
-	count=[]
-	with open('T10I4D100K.csv', 'rb') as csvfile:
+    count=[]
+    j=1
+    with open('T10I4D100K.csv', 'rb') as csvfile:
     	 spamreader = csv.reader(csvfile)
     	 for row in spamreader:
     		for i in row:
@@ -13,26 +14,37 @@ def transactions():
     			if i not in item:
     				item.append(i)
     			count.append(i)
-    		transaction.append(count)
-    		count =[]
+    		transaction.append([len(count),count,j])
+    		count =[];j=j+1
+    transaction.sort()
 
 def partition():
- 	size = []
- 	c = 0
- 	for i in range(len(transaction)):
-         size.append(len(transaction[0]))
-         for j in range(i+1,len(transaction)):
-             if size[i]== len(transaction[j]):
-                 transaction_part[c].append(size[i])
-                 if transaction[i] not in transaction_part[c]:
-                     transaction_part[c][c1].append(transaction[i])
-                 transaction_part[c].append(list(transaction[j]))
-                 c=c+1
+    size=[]
+    transaction.sort()
+    i=0
+    while i < len(transaction):
+        l=transaction[i][0]
+        size.append(l)
+        for j in range(i,len(transaction)):
+            if transaction[j][0]>transaction[i][0]:
+                i=j
+                break
+            else:
+                size.append([transaction[j][2],transaction[j][1]])
+        part.append(size)
+        size = []
+        i=i+1
+'''def gen_matrix():
+    mat=[]
+    item.sort()
+    item.insert(0,'TID')
+    mat.append(item)
+    for i in part:
+                    
+def bin_sort(n):
+    i = bisect_left(n,item,1,len(item))
+'''
 
 transactions()
 partition()
-print len(transaction[0])
-
-
-
-
+print part
